a8970971525f8579c128a48af05893fdcd0b7b16
Sesiunina Olga <sesyunina.olga7@gmail.com>
2024-06-24 22:47:20 +0300

    conection to db

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -94,10 +94,9 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
+    'default': dj_database_url.config(
+        conn_max_age=600,
+    )
 }
 
 
