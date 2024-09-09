a29ab9c207b5cbbe45fb43b8d2fbe406711ec03e
Sesiunina Olga <sesyunina.olga7@gmail.com>
2024-09-09 14:04:37 +0300

    delete user

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -1,6 +1,10 @@
 from django.contrib.auth import get_user_model
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import (
+    AuthenticationForm,
+    UserCreationForm,
+    UserChangeForm
+)
 
 
 class UserCreateForm(UserCreationForm):
@@ -94,9 +98,3 @@ class LoginUserForm(AuthenticationForm):
     class Meta:
         model = get_user_model()
         fields = ['username', 'password']
-
-
-class UserDeleteForm(forms.ModelForm):
-    class Meta:
-        model = get_user_model()
-        fields = []
--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -4,13 +4,11 @@ from django.urls import reverse_lazy, reverse
 from django.http import HttpResponseRedirect
 from django.utils.translation import gettext_lazy as _
 from task_manager.users.forms import UserCreateForm, UserUpdateForm
-# , UserDeleteForm
 from task_manager.view_mixins import IndexViewMixin
 # , CreateViewMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.contrib import messages
-# from django.contrib.auth import logout
 from django.contrib.auth.models import User
 
 
@@ -73,62 +71,7 @@ class UserDeleteView(LoginRequiredMixin, DeleteView):
         else:
             return render(request, 'users/delete.html')
 
-    # не работает, это исправить
     def get_success_url(self):
-        # messages.success(self.request,
-        #                  _('User has been deleted successfully.'))
-        return HttpResponseRedirect(reverse('index'))
-
-
-# def update_user(request, pk):
-#     user = User.objects.get(id=pk)
-#     if request.method == 'POST':
-#         form = UserCreateForm(request.POST)
-#         if form.is_valid():
-#             new_user = form.save(commit=False)
-#             new_user.set_password(form.cleaned_data['password1'])
-#             new_user.save()
-#             messages.success(request,
-#                              _('User has been updated successfully.'))
-#             return HttpResponseRedirect(reverse('index',
-#                                                 kwargs={'pk': new_user.id}))
-#     else:
-#         if not request.user.is_authenticated:
-#             messages.error(request,
-#                            _('You are not authorized please login.'))
-#             return HttpResponseRedirect(reverse('login'))
-#         else:
-#             if request.user.id != pk:
-#                 messages.error(request,
-#                                _('You do not have permission to'
-#                                  ' change another user.'))
-#                 return HttpResponseRedirect(reverse('users'))
-#             else:
-#                 form = UserCreateForm(instance=user)
-#     return render(request, 'users/update.html', {'form': form})
-
-
-# def delete_user(request, pk):
-#     if request.method == 'POST':
-#         form = UserDeleteForm(request.POST)
-#         if form.is_valid():
-#             user = request.user
-#             logout(request)
-#             user.delete()
-#             messages.success(request,
-#                              _('User has been deleted successfully.'))
-#             return HttpResponseRedirect(reverse('index'))
-#     else:
-#         if not request.user.is_authenticated:
-#             messages.error(request,
-#                            _('You are not authorized please login.'))
-#             return HttpResponseRedirect(reverse('login'))
-#         else:
-#             if request.user.id != pk:
-#                 messages.error(request,
-#                                _('You do not have permission to'
-#                                  ' change another user.'))
-#                 return HttpResponseRedirect(reverse('users'))
-#             else:
-#                 form = UserDeleteForm()
-#     return render(request, 'users/delete.html', {'form': form})
+        messages.success(self.request,
+                         _('User has been deleted successfully.'))
+        return reverse_lazy('index')
