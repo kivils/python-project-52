7d544cbb9aab48f2b2f646292ad4077e1e5c19e4
Olga <sesyunina.olga7@gmail.com>
2024-12-02 14:45:27 +0100

    executor in filter

--- task_manager/tasks/filter.py
+++ task_manager/tasks/filter.py
@@ -4,31 +4,39 @@ from .models import Task
 from task_manager.labels.models import Label
 from task_manager.statuses.models import Statuses
 from django.contrib.auth import get_user_model
+from .forms import CustomChoiceField
+
+
+class CustomExecutorFilter(django_filters.Filter):
+    field_class = CustomChoiceField
 
 
 class TaskFilter(django_filters.FilterSet):
-    executor = django_filters.ModelChoiceFilter(
+    executor = CustomExecutorFilter(
         queryset=get_user_model().objects.all(),
         label='Исполнитель',
         required=False,
         widget=forms.Select(attrs={'class': 'form-select'})
     )
-# пишет только имя, а я ожидаю еще ФАМИЛИЮ
+
     labels = django_filters.ModelChoiceFilter(
         queryset=Label.objects.all(),
         label='Метки',
         required=False,
         widget=forms.Select(attrs={'class': 'form-select'})
     )
+
     status = django_filters.ModelChoiceFilter(
         queryset=Statuses.objects.all(),
         label='Статус',
         required=False,
         widget=forms.Select(attrs={'class': 'form-select'})
     )
+
+# сделать кнопочку слева от текста
     author = django_filters.BooleanFilter(
         widget=forms.CheckboxInput(attrs={
-            'class': "form-check-input mr-3 is-valid"}),
+            'class': "form-check-input mr-3"}),
         label=("Только свои задачи"),
         method="self_tasks",
     )
