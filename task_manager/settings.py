9c6105b343f6373105f6ead9e266509577142d67
Olga <sesyunina.olga7@gmail.com>
2025-01-10 17:11:26 +0100

    return to postgres workflow

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -119,12 +119,6 @@ DATABASES = {
     )
 }
 
-if os.getenv('DB_ENGINE') == 'SQLite':
-    DATABASES['default'] = {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
